7d2bf26c1f855c9b9afe4b4660910135f1294f10
Daniel Reich <dreich@xmatters.com>
2016-09-16 17:23:09 -0700

    Major clean up of the message builder utility.

--- bin/build-messages-cmd.js
+++ bin/build-messages-cmd.js
@@ -3,14 +3,35 @@
 var AlderaanMessageBuilder = require('../');
 var minimist = require('minimist');
 var fs = require('fs');
+var os = require('os');
+var path = require('path');
 
 var argv = minimist(process.argv.slice(2), {
-    alias: { h: 'help' }
+    alias: { 
+    	h: 'help',
+    	c: 'config',
+    	t: 'templates'
+     }
 });
+
 if (argv.help) {
-    fs.createReadStream(__dirname + '/usage.txt').pipe(process.stdout);
+    fs.createReadStream(__dirname + '/messages-usage.txt').pipe(process.stdout);
     return;
 }
+var config = path.join( '.', 'conf', 'message-config.json' );
+if( argv.config ) {
+	config = argv.config;  
+}
+var templates = path.join( os.homedir(), 'message-builder',  'templates' );
+if( argv.templates ) {
+	templates = argv.templates;  
+}
 
-var configPath = process.argv[2];
-AlderaanMessageBuilder.buildMessagesFromConfig( configPath );
+var configPath = config;
+var templatesPath = templates;
+try {
+	AlderaanMessageBuilder.buildMessagesFromConfig( configPath, templatesPath );
+	console.log( 'SUCCESS: Generated Messages based on configuration, templates and metadata.' );
+} catch ( e ) {
+	console.log( 'ERROR: ' + e );
+}
